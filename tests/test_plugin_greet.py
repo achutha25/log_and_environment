ac619bf2d673a4b7574ee6350d8b8b70c533c6d2
Achutha Mullapudi <achuthamullapudi@mac.mynetworksettings.com>
2025-03-04 14:05:56 -0500

    fixes

--- tests/test_plugin_greet.py
+++ tests/test_plugin_greet.py
@@ -14,7 +14,7 @@ def test_app_greet_command(capfd, monkeypatch):
     assert e.value.code == 0, "The app did not exit as expected"
 
     # Capture the output from the 'greet' command
-     out, err = capfd.readouterr()
+    out, err = capfd.readouterr()
     
     # Assert that 'Hello, World!' was printed to stdout
     assert "Hello, World!" in out, "The 'greet' command did not produce the expected output."
