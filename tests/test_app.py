56cb01173f55e92847b03af3f0556ee2f7f44fc9
Achutha Mullapudi <achuthamullapudi@mac.mynetworksettings.com>
2025-03-04 13:57:37 -0500

    fixes for linting and cov

--- tests/test_app.py
+++ tests/test_app.py
@@ -28,7 +28,7 @@ def test_app_start_unknown_command(capfd, monkeypatch):
 
     app = App()
     
-    with pytest.raises(SystemExit) as excinfo:
+    with pytest.raises(SystemExit):
         app.start()
     
     # Optionally, check for specific exit code or message
